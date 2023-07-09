4379c74ecff9154bd890c2250016dbfc6b0e660d
David Esteves Rodrigues <dro135638@stud.gibb.ch>
2023-07-09 16:37:36 +0200

    Home Page added boxes and desc.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <div className='box'>
+      <div className='content_box'>
+        <p className='box_title'>Lebenslauf</p>
+      </div>
+      <p className='description'>Meine bisherigen schulischen Werdegänge mit allen relevanten Informationen</p>
+    </div>
+
+    <hr className='line'></hr>
+
+    <div className='box_switch'>
+      <p className='description_switch'>Meine bisherigen schulischen Werdegänge mit allen relevanten Informationen</p>
+      <div className='content_box_switch'>
+        <p className='box_title'>Lebenslauf</p>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
