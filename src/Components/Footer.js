fa2e877e773e3d60345a650c06162cf6bee88d50
David Esteves Rodrigues <dro135638@stud.gibb.ch>
2023-07-09 20:51:40 +0200

    bug fixes and added footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div>
+            <footer id='footer'>
+                    <div id='copyright'>
+                        <p>Copyright © 2023 David Esteves Rodrigues. All rights reserved.</p>
+                    </div>
+                <p id='impressum'>Impressum</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './Components/Navbar.js'
 import reportWebVitals from './reportWebVitals';
+import Footer from './Components/Footer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,9 +21,37 @@ root.render(
     <div className='box_switch'>
       <p className='description_switch'>Meine bisherigen schulischen Werdegänge mit allen relevanten Informationen</p>
       <div className='content_box_switch'>
-        <p className='box_title'>Lebenslauf</p>
+        <p className='box_title'>Stärken</p>
       </div>
     </div>
+
+    <hr className='line'></hr>
+
+    <div className='box'>
+      <div className='content_box'>
+        <p className='box_title'>IT-Projekte</p>
+      </div>
+      <p className='description'>Meine bisherigen schulischen Werdegänge mit allen relevanten Informationen</p>
+    </div>
+
+    <hr className='line'></hr>
+
+    <div className='box_switch'>
+      <p className='description_switch'>Meine bisherigen schulischen Werdegänge mit allen relevanten Informationen</p>
+      <div className='content_box_switch'>
+        <p className='box_title'>Interessen</p>
+      </div>
+    </div>
+
+    <hr className='line'></hr>
+
+    <div className='box'>
+      <div className='content_box'>
+        <p className='box_title'>Portfolio</p>
+      </div>
+      <p className='description'>Meine Noten vom 1. bis zum 3. Schuljahr von der gibb und von bwd</p>
+    </div>
+    <Footer />
   </React.StrictMode>
 );
 
