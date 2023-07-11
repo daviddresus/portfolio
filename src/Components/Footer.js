c522c7f269d61b5e6f18eff6f044cbb83ddf8a2c
David Esteves Rodrigues <dro135638@stud.gibb.ch>
2023-07-11 11:39:30 +0200

    footer changes (not done yet)

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div>
             <footer id='footer'>
                     <div id='copyright'>
-                        <p>Copyright © 2023 David Esteves Rodrigues. All rights reserved.</p>
+                        <p>Copyright © 2023 David Esteves Rodrigues</p>
                     </div>
                 <p id='impressum'>Impressum</p>
             </footer>
