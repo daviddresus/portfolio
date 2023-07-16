e2c8182ad2f4f2f55cbd24bee1f1bc1c180ba537
David Rodrigues <dro135638@stud.gibb.ch>
2023-07-16 11:31:57 +0200

    started Interessen Page (no functionality yet)

--- src/Pages/Interessen/Interessen.js
+++ src/Pages/Interessen/Interessen.js
@@ -3,6 +3,22 @@ import Title from '../../Components/Title/Title.js';
 
 export default function Interessen() {
   return (
-    <Title text="Interessen"/>
+    <div>
+      <Title text="Interessen" />
+
+      <div id='all_interest_boxes'>
+        <div className='interest_box'>
+          <p className='intereset_title'>Frontend</p>
+        </div>
+
+        <div className='interest_box'>
+          <p className='intereset_title'>Game Development</p>
+        </div>
+
+        <div className='interest_box'>
+          <p className='intereset_title'>Editing</p>
+        </div>
+      </div>
+    </div>
   )
 }
