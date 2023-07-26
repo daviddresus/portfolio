130cf6476726cb2b736e7abb9d272d7b101b9ba8
David Rodrigues <dro135638@stud.gibb.ch>
2023-07-26 12:39:55 +0200

    started the implementation of functionality on the Interessen Page

--- src/Pages/Interessen/Interessen.js
+++ src/Pages/Interessen/Interessen.js
@@ -1,35 +1,48 @@
 import './Interessen.css'
 import Title from '../../Components/Title/Title.js';
-
-const goLeft = () => {
-  document.getElementById('all_interest_boxes').style = "transform: translateX(27%); transition: 0.5s ease-in-out;";
-}
-
-const goRight = () => {
-  document.getElementById('all_interest_boxes').style = "transform: translateX(-27%); transition: 0.5s ease-in-out;";
-}
+import { useState } from 'react';
 
 export default function Interessen() {
+
+  const [centeredIndex, setCenteredIndex] = useState(1);
+
+  const items = [
+    { title: 'Frontend' },
+    { title: 'Game Development' },
+    { title: 'Editing' },
+    // Add more items as needed
+  ];
+
+  const goLeft = () => {
+    const nextIndex = (centeredIndex + items.length - 1) % items.length;
+    setCenteredIndex(nextIndex);
+  };
+  
+  const goRight = () => {
+    const nextIndex = (centeredIndex + 1) % items.length;
+    setCenteredIndex(nextIndex);
+  };
+
+  const calculateMargin = (index) => {
+    const containerWidth = 100 / 3 // 3 items displayed at a time
+    const centeredItemOffset = index * containerWidth;
+    return `calc(50% - ${centeredItemOffset}%)`;
+  };
+
   return (
     <div>
       <Title text="Interessen" />
 
       <img className='button' src='Images/buttonleft.svg' alt='Left Button' onClick={goLeft} />
 
-      <div id='all_interest_boxes'>
-
-        <div className='interest_box'>
-          <p className='intereset_title'>Frontend</p>
-        </div>
-
-        <div className='interest_box'>
-          <p className='intereset_title'>Game Development</p>
-        </div>
-
-        <div className='interest_box'>
-          <p className='intereset_title'>Editing</p>
-        </div>
-
+      <div id='all_interest_boxes' style={{ marginLeft: calculateMargin(centeredIndex) }}>
+        {items.map((item, index) => (
+            <div
+              key={index}
+              className='interest_box'>
+              <p className='interest_title'>{item.title}</p>
+            </div>
+          ))}
       </div>
 
       <img className='button' src='Images/buttonright.svg' alt='Right Button' onClick={goRight} />
