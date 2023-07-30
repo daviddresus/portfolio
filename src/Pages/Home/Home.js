d8c71f55db3bf551b51d8bd4c3176da66b5cedad
David Rodrigues <dro135638@stud.gibb.ch>
2023-07-30 19:09:54 +0200

    interessen page has proper animation when using the buttons

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,9 +4,11 @@ export default function Home() {
     return (
         <div id='all_content_boxes'>
             <div className='box'>
-                <div className='content_box'>
-                    <p className='box_title'>Lebenslauf</p>
-                </div>
+                <a href='/lebenslauf'>
+                    <div className='content_box'>
+                        <p className='box_title'>Lebenslauf</p>
+                    </div>
+                </a>
                 <p className='description'>Meine bisherigen schulischen Werdegänge mit allen relevanten Informationen</p>
             </div>
 
@@ -14,17 +16,21 @@ export default function Home() {
 
             <div className='box_switch'>
                 <p className='description_switch'>bla bla</p>
-                <div className='content_box_switch'>
-                    <p className='box_title'>Stärken</p>
-                </div>
+                <a href='/staerken'>
+                    <div className='content_box_switch'>
+                        <p className='box_title'>Stärken</p>
+                    </div>
+                </a>
             </div>
 
             <hr className='line'></hr>
 
             <div className='box'>
-                <div className='content_box'>
-                    <p className='box_title'>IT-Projekte</p>
-                </div>
+                <a href='/itprojekte'>
+                    <div className='content_box'>
+                        <p className='box_title'>IT-Projekte</p>
+                    </div>
+                </a>
                 <p className='description'>bla bla</p>
             </div>
 
@@ -32,17 +38,21 @@ export default function Home() {
 
             <div className='box_switch'>
                 <p className='description_switch'>bla bla</p>
-                <div className='content_box_switch'>
-                    <p className='box_title'>Interessen</p>
-                </div>
+                <a href='/interessen'>
+                    <div className='content_box_switch'>
+                        <p className='box_title'>Interessen</p>
+                    </div>
+                </a>
             </div>
 
             <hr className='line'></hr>
 
             <div className='box'>
-                <div className='content_box'>
-                    <p className='box_title'>Portfolio</p>
-                </div>
+                <a href='/portfolio'>
+                    <div className='content_box'>
+                        <p className='box_title'>Portfolio</p>
+                    </div>
+                </a>
                 <p className='description'>Meine Noten vom 1. bis zum 3. Schuljahr von der gibb und von bwd</p>
             </div>
         </div>
--- src/Pages/Interessen/Interessen.js
+++ src/Pages/Interessen/Interessen.js
@@ -1,51 +1,80 @@
 import './Interessen.css'
 import Title from '../../Components/Title/Title.js';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Interessen() {
 
-  const [centeredIndex, setCenteredIndex] = useState(1);
-
   const items = [
-    { title: 'Frontend' },
-    { title: 'Game Development' },
-    { title: 'Editing' },
+    { title: 'Frontend', id: '1' },
+    { title: 'Game Development', id: '2' },
+    { title: 'Editing', id: '3' }
     // Add more items as needed
   ];
 
-  const goLeft = () => {
-    const nextIndex = (centeredIndex + items.length - 1) % items.length;
-    setCenteredIndex(nextIndex);
-  };
-  
-  const goRight = () => {
-    const nextIndex = (centeredIndex + 1) % items.length;
-    setCenteredIndex(nextIndex);
+  var placement = 0;
+  var item = 2;
+
+  const transparency = () => {
+    if (items.length - item !== 0) {
+      document.getElementById(item).style = 'opacity: 1; transition: ease-in-out 0.5s';
+      document.getElementById(item + 1).style = 'opacity: 0.4; transition: ease-in-out 0.5s';
+    }
+
+    if (items.length - item !== 0 && items.length - item !== 1) {
+      document.getElementById(item + 2).style = 'opacity: 0; transition: ease-in-out 0.5s';
+    }
+
+    if (item !== 1) {
+      document.getElementById(item).style = 'opacity: 1; transition: ease-in-out 0.5s';
+      document.getElementById(item - 1).style = 'opacity: 0.4; transition: ease-in-out 0.5s';
+    }
+
+    if (item !== 1 && item !== 2) {
+      document.getElementById(item - 2).style = 'opacity: 0; transition: ease-in-out 0.5s';
+    }
   };
 
-  const calculateMargin = (index) => {
-    const containerWidth = 100 / 3 // 3 items displayed at a time
-    const centeredItemOffset = index * containerWidth;
-    return `calc(50% - ${centeredItemOffset}%)`;
+  //makes sure that the const transparency is executed even if no button was clicked
+  useEffect(() => {
+    transparency();
+  }, []);
+
+  const goLeft = () => {
+    if (item !== 1) {
+      placement = placement + 27.2;
+      document.getElementById('all_interest_boxes').style = `transform: translateX(${placement}%); transition: ease-in-out 0.5s`;
+      item = item - 1;
+      transparency();
+    }
+  };
+
+  const goRight = () => {
+    if (items.length - item !== 0) {
+      placement = placement - 27.2;
+      document.getElementById('all_interest_boxes').style = `transform: translateX(${placement}%); transition: ease-in-out 0.5s`;
+      item = item + 1;
+      transparency();
+    }
   };
 
   return (
     <div>
       <Title text="Interessen" />
+      <div id='slider'>
+        <img id='left_button' src='Images/buttonleft.svg' alt='Left Button' onClick={goLeft} />
 
-      <img className='button' src='Images/buttonleft.svg' alt='Left Button' onClick={goLeft} />
-
-      <div id='all_interest_boxes' style={{ marginLeft: calculateMargin(centeredIndex) }}>
-        {items.map((item, index) => (
+        <div id='all_interest_boxes'>
+          {items.map((item, index) => (
             <div
               key={index}
-              className='interest_box'>
+              className='interest_box' id={item.id}>
               <p className='interest_title'>{item.title}</p>
             </div>
           ))}
-      </div>
+        </div>
 
-      <img className='button' src='Images/buttonright.svg' alt='Right Button' onClick={goRight} />
+        <img id='right_button' src='Images/buttonright.svg' alt='Right Button' onClick={goRight} />
+      </div>
 
       <div className='project_box'></div>
     </div>
