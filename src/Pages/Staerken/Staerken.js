e017867331b00ead9de8316c9b88e70d40df4431
David Rodrigues <dro135638@stud.gibb.ch>
2023-08-01 21:21:43 +0200

    Started Stärken Page by adding a functional and responsive grid to the page (knowledge bar missing)

--- src/Pages/Staerken/Staerken.js
+++ src/Pages/Staerken/Staerken.js
@@ -2,7 +2,39 @@ import './Staerken.css'
 import Title from '../../Components/Title/Title.js';
 
 export default function Staerken() {
+
+  const items = [
+    { title: 'HTML', id: '1' },
+    { title: 'CSS', id: '2' },
+    { title: 'JS', id: '3' },
+    { title: 'Java', id: '4' },
+    { title: 'PHP', id: '5' },
+    { title: 'Java', id: '6' },
+    { title: 'JS', id: '3' },
+    { title: 'Java', id: '4' },
+    { title: 'PHP', id: '5' },
+    { title: 'Java', id: '6' },
+    { title: 'JS', id: '3' },
+    { title: 'Java', id: '4' },
+    { title: 'PHP', id: '5' },
+    { title: 'Java', id: '6' },
+    // Add more items as needed
+  ];
+
   return (
-    <Title text="Stärken"/>
+    <div>
+      <Title text="Stärken" />
+
+      <div id='all_strength_boxes'>
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className='strength_box' id={item.id}>
+            <p className='strength_title'>{item.title}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
