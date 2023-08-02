8c6fc7d133473303f7d0e9c9ff48105fddab727a
David Rodrigues <dro135638@stud.gibb.ch>
2023-08-02 19:21:15 +0200

    added little animation to content box in interessen Page and finished Stärken Page (content missing)

--- src/Pages/Staerken/Staerken.js
+++ src/Pages/Staerken/Staerken.js
@@ -4,20 +4,14 @@ import Title from '../../Components/Title/Title.js';
 export default function Staerken() {
 
   const items = [
-    { title: 'HTML', id: '1' },
-    { title: 'CSS', id: '2' },
-    { title: 'JS', id: '3' },
-    { title: 'Java', id: '4' },
-    { title: 'PHP', id: '5' },
-    { title: 'Java', id: '6' },
-    { title: 'JS', id: '3' },
-    { title: 'Java', id: '4' },
-    { title: 'PHP', id: '5' },
-    { title: 'Java', id: '6' },
-    { title: 'JS', id: '3' },
-    { title: 'Java', id: '4' },
-    { title: 'PHP', id: '5' },
-    { title: 'Java', id: '6' },
+    { title: 'HTML', percentage: '85', id: '1' },
+    { title: 'CSS', percentage: '80', id: '2' },
+    { title: 'JS', percentage: '60', id: '3' },
+    { title: 'Java', percentage: '60', id: '4' },
+    { title: 'PHP', percentage: '45', id: '5' },
+    { title: 'C#', percentage: '40', id: '6' },
+    { title: 'Git', percentage: '65', id: '7' },
+    { title: 'Python', percentage: '50', id: '8' }
     // Add more items as needed
   ];
 
@@ -31,6 +25,9 @@ export default function Staerken() {
             key={index}
             className='strength_box' id={item.id}>
             <p className='strength_title'>{item.title}</p>
+            <div id='progressbar'>
+              <div id='percentage' style={{ width: `${item.percentage}%`}}></div>
+            </div>
           </div>
         ))}
       </div>
