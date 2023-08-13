0e28a600f3128441622c093e053f2ac3437b75b7
David Esteves Rodrigues <dro135638@stud.gibb.ch>
2023-08-13 17:07:08 +0200

    Added an Embed for the Lebenslauf.pdf that I created (if the embed can't be displayed, then a message with a donwload link will appear

--- src/Pages/Lebenslauf/Lebenslauf.js
+++ src/Pages/Lebenslauf/Lebenslauf.js
@@ -3,6 +3,17 @@ import Title from '../../Components/Title/Title.js';
 
 export default function Lebenslauf() {
   return (
-    <Title text="Lebenslauf"/>
+    <div>
+      <Title text="Lebenslauf" />
+      <object data='Files/Lebenslauf.pdf' type='application/pdf' title="Lebenslauf">
+        <div className='pdf_error_msg'>
+          <p>Ihr Webbrowser kann diese PDF-Datei aktuell nicht anzeigen. Stattdessen können Sie eine Kopie davon herunterladen mit dem unten stehenden Link:</p>
+        </div>
+
+        <div className='pdf_download_link'>
+          <a href="Files/Lebenslauf.pdf" download="Lebenslauf">Clicken Sie Hier um Ihren download zu starten</a>
+        </div>
+      </object>
+    </div>
   )
 }
