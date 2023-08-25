a6a76a84cbeaf9f088d2bf78d9e1d266e1f4afce
David Rodrigues <dro135638@stud.gibb.ch>
2023-08-25 09:08:10 +0200

    added svg for linkedin and github (redirect missing)

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -13,6 +13,8 @@ function Footer() {
         <div>
             <Impressum />
             <footer id='footer'>
+                <img src='Images/linkedin.svg'></img>
+                <img src='Images/github.svg'></img>
                 <div id='copyright'>
                     <p><span>Copyright © 2023 </span><span>David Esteves Rodrigues</span></p>
                 </div>
