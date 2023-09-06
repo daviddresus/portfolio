bf8ce11563f413d487e488ef7c38795dd524ef37
David Rodrigues <dro135638@stud.gibb.ch>
2023-09-06 14:08:20 +0200

    added updated version of Lebenslauf

--- src/Components/DropDown/Dropdown.js
+++ src/Components/DropDown/Dropdown.js
@@ -26,15 +26,10 @@ export default function Dropdown({ content, title, fileLoc, fileName }) {
                 <div className={styles.dropdown_button}>
                     <h2>{title}</h2><img src='Images/dropdown.svg' alt='Dropdown Icon' id={styles.dropdown_icon} style={svgStyle} />
                 </div>
-<<<<<<< HEAD
-                <div className={styles.download_button}>
-                    <p>text</p>
-=======
                 <div className={`${styles.download_button} ${open ? styles.showDownload : styles.hideDownload}`}>
                     <a href={fileLoc} download={fileName}>
                         <img src='Images/download.svg' alt='download' />
                     </a>
->>>>>>> e25dee126aa62cf12fd1c45be35b4d4f8e794373
                 </div>
             </div>
             <div className={`${styles.content} ${open ? styles.contentOpen : styles.contentClosed}`}>
